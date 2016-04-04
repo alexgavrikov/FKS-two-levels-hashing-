61814f85fd99662a811d3e8cfe75c3def346c2f2
alexgavrikov <alex-gavrikov@mail.ru>
2016-04-04 18:16:02 +0300

    new separate files with old code
    for flexibility

---
+++ core_function.h
@@ -0,0 +1,18 @@
+/*
+ * core_function.h
+ *
+ *  Created on: 04.04.2016
+ *      Author: user
+ */
+
+#ifndef CORE_FUNCTION_H_
+#define CORE_FUNCTION_H_
+
+#include <vector>
+
+std::vector<bool> CheckIfElementsBelongToSet(
+    const std::vector<int>& set_elements,
+    const std::vector<int>& elements_to_check);
+
+
+#endif /* CORE_FUNCTION_H_ */
